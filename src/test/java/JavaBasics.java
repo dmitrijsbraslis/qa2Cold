5bf4a5ec333eb6eadbe3f550bde0f82facd1c969
dmitrijs.braslis <dmitrijs.braslis@meawallet.com>
2018-10-03 21:24:12 +0300

    some comment here

--- src/test/java/JavaBasics.java
+++ src/test/java/JavaBasics.java
@@ -1,2 +1,17 @@
+import org.junit.jupiter.api.Test;
+
 public class JavaBasics {
+    private String text = "Sum is: ";
+
+    @Test
+    public void sumTwoDigits() {
+        sumAndPrint(45, 90);
+        sumAndPrint(1, 3);
+        sumAndPrint(405, 900);
+    }
+
+    private void sumAndPrint(Integer a, Integer b) {
+        Integer c = a + b + b;
+        System.out.println(text + "for int: " + c);
+    }
 }
